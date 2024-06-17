559c8c6235aad3759817cbbd50112f8c4667ffae
Michiel de Jong <michiel@pondersource.com>
2024-06-17 11:50:21 +0200

    take first issue from github and create it on jira

--- Milestone 1/1c - Feasibility investigation of live multi-homed data/exploration/sync.sh
+++ Milestone 1/1c - Feasibility investigation of live multi-homed data/exploration/sync.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 gh api /issues --method GET | python -m json.tool > gh.json
-deno run -A ./sync.ts > jira.json
-curl --request POST \
-  --url 'https://fedtt.atlassian.net/rest/api/3/issue' \
-  --user "michiel@unhosted.org:$ATLASSIAN" \
-  --header 'Accept: application/json' \
-  --header 'Content-Type: application/json' \
-  --data @jira-example.json > jira-api-response-example.json
+deno run -A ./sync.ts
+
+
+# curl --request POST \
+#   --url 'https://fedtt.atlassian.net/rest/api/3/issue' \
+#   --user "michiel@unhosted.org:$ATLASSIAN" \
+#   --header 'Accept: application/json' \
+#   --header 'Content-Type: application/json' \
+#   --data @jira-example.json > jira-api-response-example.json
 
 # curl --request GET \
 #   --url 'https://fedtt.atlassian.net/rest/api/3/issue/KAN-1' \
--- Milestone 1/1c - Feasibility investigation of live multi-homed data/exploration/sync.ts
+++ Milestone 1/1c - Feasibility investigation of live multi-homed data/exploration/sync.ts
@@ -1,5 +1,40 @@
-import { readFileSync } from 'node:fs'
+import { readFileSync } from "node:fs";
+import * as base64 from "jsr:@std/encoding/base64";
 
-const text = readFileSync('./gh.json')
-const obj = JSON.parse(text)
-console.log(obj[0])
+const JIRA_ORG = `fedtt`;
+const JIRA_USER = `michiel@unhosted.org`;
+const JIRA_TOKEN = Deno.env.get(`ATLASSIAN`);
+
+const JIRA_CREATE_ISSUE_URL = `https://${JIRA_ORG}.atlassian.net/rest/api/3/issue`;
+const JIRA_CREATE_ISSUE_OPTIONS = {
+  method: "POST",
+  headers: {
+    Authorization: `Basic ${btoa(JIRA_USER + ":" + JIRA_TOKEN)}`,
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: 'fill me in'
+};
+
+const text = readFileSync("./gh.json").toString();
+const obj = JSON.parse(text);
+
+async function postToJira(issue: { title: string }) {
+  const options = JIRA_CREATE_ISSUE_OPTIONS;
+  options.body = JSON.stringify({
+    "fields": {
+        "project": {
+            "id": "10000"
+        },
+        "issuetype": {
+            "id": "10001"
+        },
+        "summary": issue.title
+    }
+  });
+  console.log("POSTING to JIRA", JSON.parse(options.body));
+  const res = await fetch(JIRA_CREATE_ISSUE_URL, options);
+  return res.json();
+}
+
+console.log(await postToJira(obj[0]));
