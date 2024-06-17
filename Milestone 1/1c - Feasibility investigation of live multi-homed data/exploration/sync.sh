55442c5ca29b0c14b32c2c93cd27fac028c97624
Michiel de Jong <michiel@pondersource.com>
2024-06-17 10:54:21 +0200

    You do not have permission to create issues in this project.

--- Milestone 1/1c - Feasibility investigation of live multi-homed data/exploration/sync.sh
+++ Milestone 1/1c - Feasibility investigation of live multi-homed data/exploration/sync.sh
@@ -2,3 +2,9 @@
 
 gh api /issues --method GET | python -m json.tool > gh.json
 deno run -A ./sync.ts > jira.json
+curl --request POST \
+  --url 'https://fedtt.atlassian.net/rest/api/3/issue' \
+  --user 'michiel@unhosted.org:$ATLASSIAN' \
+  --header 'Accept: application/json' \
+  --header 'Content-Type: application/json' \
+  --data @jira-example.json
