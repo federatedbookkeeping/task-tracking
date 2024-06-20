3f1b46ee754024352e267932acbdb65f0a0f1a13
Michiel de Jong <michiel@pondersource.com>
2024-06-20 13:45:41 +0200

    import from GitHub to DXOS ECHO, ref #28

--- Milestone 3/bridgeBot.ts
+++ Milestone 3/bridgeBot.ts
@@ -42,32 +42,43 @@ async function readGitHub(): Promise<GitHubIssue[]> {
   return githubArr;
 }
 
+async function update(client: Client, githubArr: GitHubIssue[]) {
+  console.log('start update');
+  const promises = githubArr.map(async (issue: GitHubIssue) => {
+    console.log(`exists query start ${issue.node_id}`);
+    const exists = await client.spaces.default.db.query({ node_id: issue.node_id }).run();
+    console.log(`exists query done ${issue.node_id}`);
+    if (exists.results.length == 0) {
+      console.log(`Adding ${issue.node_id}`, issue.title, issue.node_id);
+      await client.spaces.default.db.add(create(Expando, { type: 'task', title: issue.title, node_id: issue.node_id, isCompleted: false }));
+    } else if ((exists.results[0] as unknown as { object: { title: string } }).object.title !== issue.title) {
+      console.log(`Updating ${issue.node_id}`, (exists.results[0] as unknown as { object: { title: string } }).object, issue.title);
+      (exists.results[0] as unknown as { object: { title: string } }).object.title = issue.title      
+    } else {
+      console.log(`Nothing to do for ${issue.node_id}`);
+    }
+  });
+  console.log('awaiting promises');
+  await Promise.all(promises);
+  console.log('update done');
+}
+
 const main = async () => {
   const client = new Client();
   await client.initialize();
   if (!client.halo.identity.get()) await client.halo.createIdentity();
   await client.spaces.isReady.wait();
   const githubArr = await readGitHub();
-  const promises = githubArr.map(async(issue: GitHubIssue) => {
-    const exists = await client.spaces.default.db.query({ node_id: issue.node_id }).run();
-    if (exists.results.length == 0) {
-      console.log(`Adding ${issue.node_id}`);
-      await client.spaces.default.db.add(create(Expando, { type: 'task', title: issue.title, node_id:  issue.node_id }));
-    } else if ((exists.results[0] as unknown as { title: string}).title !== issue.title) {
-      console.log(`Updating ${issue.node_id}`);
-      (exists.results[0] as unknown as { title: string}).title = issue.title      
-    } else {
-      console.log(`Nothing to do for ${issue.node_id}`);
-    }
-  });
-  await Promise.all(promises);
+  console.log('First run');
+  await update(client, githubArr);  
+  console.log('Second run');
+  await update(client, githubArr);  
   client.destroy();
 };
 
-console.log('First run');
 main().then(() => {
-  console.log('Second run');
-  return main();
+  // console.log('Second run');
+  // return main();
 }).then(() => {
   // FIXME: https://discord.com/channels/837138313172353095/1253303287998976030/1253303287998976030 
   console.log("done, but process doesn't exit?");
